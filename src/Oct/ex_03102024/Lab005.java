c56641d0d94b634d26e05ca2b83be6a6a97c8d04
ramal <ramal.jafri@gmail.com>
2024-10-07 21:27:30 -0400

    all files

--- src/Oct/ex_03102024/Lab005.java
+++ src/Oct/ex_03102024/Lab005.java
@@ -1,4 +1,8 @@
 package Oct.ex_03102024;
 
 public class Lab005 {
+    public static void main(String[] args) {
+
+    }
+
 }
---
+++ src/Oct/ex_08102024/Lab006.java
@@ -0,0 +1,14 @@
+package Oct.ex_08102024;
+
+public class Lab006 {
+    public static void main(String[] args) {
+        //data type variable_name = variable_value
+
+        int age=65;
+
+        // data type -> int - whole numbers (not decimals)
+        //age -> variable name / identifier
+        // = --> assignment operator
+        // 65 --> literal
+    }
+}
